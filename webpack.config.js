5f7a24925b00d7daa34bf7777130842fbac6c64e
Abibibi <abeba.ngwe@gmail.com>
2020-05-07 16:48:32 +0200

    with cacheDirectory

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
             {
                 test: /\.(js)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                loader: 'babel-loader?optional=runtime&cacheDirectory'
             },
             {
                 test: /\.css$/,
